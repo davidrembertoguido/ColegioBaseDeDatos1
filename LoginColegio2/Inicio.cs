399b68d66087166e07652fe1f98aef591f6d7992
davidrembertoguido <davidrembertoguido@gmail.com>
2024-12-03 14:28:06 -0600

    gg

--- LoginColegio2/Inicio.cs
+++ LoginColegio2/Inicio.cs
@@ -12,9 +12,43 @@ namespace LoginColegio2
 {
     public partial class Inicio : Form
     {
+        private Panel leftBordenBtn;
+        private Form currenChildForm;
         public Inicio()
         {
             InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenChildForm(Form ChildForm)
+        {
+            if (currenChildForm != null)
+            {
+                currenChildForm.Close();
+            }
+            currenChildForm = ChildForm;
+            ChildForm.TopLevel = false;
+            ChildForm.FormBorderStyle = FormBorderStyle.None;
+            ChildForm.Dock = DockStyle.Fill;
+            PnlContenedor.Controls.Add(ChildForm);
+            PnlContenedor.Tag = ChildForm;
+            ChildForm.BringToFront();
+            ChildForm.Show();
+            btnGuardar.Text = ChildForm.Text;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
